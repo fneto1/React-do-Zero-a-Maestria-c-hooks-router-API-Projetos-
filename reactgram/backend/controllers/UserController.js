cc55fd0332208640a85d0cbf41ecf93676054bfc
fneto1 <fasneto2@gmail.com>
2023-06-07 15:08:04 -0300

    Atualização: backend finalizado

--- reactgram/backend/controllers/UserController.js
+++ reactgram/backend/controllers/UserController.js
@@ -3,28 +3,29 @@ const User = require("../models/User")
 
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const mongoose = require("mongoose")
 
-const jwtSecret = process.env.JWT_SECRET 
+const jwtSecret = process.env.JWT_SECRET
 
 // genereate user token
 const generateToken = (id) => {
     return jwt.sign(
-        {id},
+        { id },
         jwtSecret,
-        {expiresIn: "7d"}
+        { expiresIn: "7d" }
     )
 }
 
 //register user and signIn
 const register = async (req, res) => {
-    
-    const {name, email, password} = req.body;
+
+    const { name, email, password } = req.body;
 
     //check if user exists
-    const user = await User.findOne({email})
+    const user = await User.findOne({ email })
 
-    if(user){
-        res.status(422).json({errors: ["Por favor, utilize outro e-mail"]})
+    if (user) {
+        res.status(422).json({ errors: ["Por favor, utilize outro e-mail"] })
     }
 
     //Generate password hash
@@ -40,8 +41,8 @@ const register = async (req, res) => {
 
     //if user was created succesfully, return token
 
-    if(!newUser){
-        res.status(422).json({errors: ["Houve um erro, por favor tente novamente mais tarde."]})
+    if (!newUser) {
+        res.status(422).json({ errors: ["Houve um erro, por favor tente novamente mais tarde."] })
 
         return
     }
@@ -56,10 +57,103 @@ const register = async (req, res) => {
 
 
 const login = async (req, res) => {
-    res.send("login")
+
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    //Check if user exists
+    if (!user) {
+        res.status(404).json({ errors: ["Usuário não encontrado"] })
+        return
+    }
+
+    if (!(await bcrypt.compare(password, user.password))) {
+        res.status(422).json({ errors: ["Senha inválida"] })
+        return
+    }
+
+    //return user with token
+    res.status(201).json({
+        _id: user._id,
+        profileImage: user.profileImage,
+        token: generateToken(user._id)
+    })
+}
+
+//Get current logged in user
+const getCurrentUser = async (req, res) => {
+    const user = req.user
+
+    res.status(200).json(user)
+}
+
+//update an user
+const update = async (req, res) => {
+
+    const { name, password, bio } = req.body
+
+    let profileImage = null
+
+    if (req.file) {
+        profileImage = req.file.filename
+    }
+
+    const reqUser = req.user
+
+    const user = await User.findById(reqUser._id).select("-password")
+
+    if (name) {
+        user.name = name
+    }
+
+    if (password) {
+        //Generate password hash
+        const salt = await bcrypt.genSalt()
+        const passwordHash = await bcrypt.hash(password, salt)
+
+        user.password = passwordHash
+    }
+
+    if (profileImage) {
+        user.profileImage = profileImage
+    }
+
+    if (bio) {
+        user.bio = bio
+    }
+
+    await user.save()
+
+    res.status(200).json(user)
+
+}
+
+//Get user by id
+const getUserById = async (req, res) => {
+
+    const { id } = req.params
+
+    try {
+        const user = await User.findById(id).select("-password")
+
+        //check if user exists
+        if (!user) {
+            res.status(404).json({ errors: ["Usuário não encontrado."] })
+        }
+
+        res.status(200).json(user)
+    } catch (error) {
+        res.status(404).json({ errors: ["Usuário não encontrado."] })
+    }
+
+
 }
 
 module.exports = {
     register,
     login,
+    getCurrentUser,
+    update,
+    getUserById
 }
--- reactgram/backend/middlewares/userValidations.js
+++ reactgram/backend/middlewares/userValidations.js
@@ -47,7 +47,23 @@ const loginValidation = () => {
     ]
 }
 
+const userUpdateValidation = () => {
+
+    return[
+        body("name")
+            .optional()
+            .isLength({min: 3})
+            .withMessage("O nome precisa ter pelo menos 3 caracteres"),
+
+        body("password")
+            .optional()
+            .isLength({min: 5})
+            .withMessage("A senha precisa ter pelo no minimo 5 caracteres"),
+    ]
+}
+
 module.exports = {
     userCreateValidation,
-    loginValidation
+    loginValidation,
+    userUpdateValidation
 }
